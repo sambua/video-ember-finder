419de97ddde3fd0cfca9c3b625e0546dc507bd06
Rashad Aliyev <aliyev.resad@gmail.com>
2016-10-12 23:42:54 +0400

    Fully transformed for some type video service providers

--- server.js
+++ server.js
@@ -9,9 +9,17 @@ var csvWriter = require('csv-write-stream')
 var writer = csvWriter({ headers: ["Company Name", "Home Page", "Video Service"]});
 
 // input file source
-var companies_file = './tmp/organizations.json';
+//var companies_file = './tmp/organizations.json';
+var companies_file = './tmp/test_org.json';
 //var inputFile = './tmp/searched_container.txt';  // export file source
 var inputFile = './tmp/searched_container.csv';
+
+const YOUTUBE = 'youtube';
+const VIMEO   = 'vimeo';
+const WISTIA  = 'wistia';
+const KULTURA = 'kultura';
+const JWPLAYER = 'jwplayer';
+const VIDEOJS = 'videojs';
 
 writer.pipe(fs.createWriteStream(inputFile));
 
@@ -19,7 +27,7 @@ writer.pipe(fs.createWriteStream(inputFile));
 // Empty file content
 fs.writeFile(inputFile, '');
 var searchindContent = [
-  '//youtube', '//vimeo', '.mp4', '.ogg', '.webm', '//www.youtube', '//www.vimeo', '<video', 'video'
+  '//youtube', '//vimeo', '.mp4', '.ogg', '.webm', '//www.youtube', '//www.vimeo', '<video'
 ];
 
 
@@ -47,22 +55,49 @@ function processCrawlingCustomer(data, es ) {
     if( !error && response.statusCode === 200 ) {
       
       var $ = cheerio.load(html);
-      var isVideoContainer = false;
+      var isVideoContainer = true;
+      var serviceProvider = '';
 
       // $('body').filter(function() {
       //   var content = $(this);
       // });
+
+      switch ( searchingVideoContetn($) ) {
+        case YOUTUBE:
+          serviceProvider = 'YOUTUBE'; 
+          break;
+        case VIMEO:
+          serviceProvider = 'VIMEO';
+          break;
+        case WISTIA:
+          serviceProvider = "WISTIA";
+          break;
+        case KULTURA:
+          serviceProvider = 'KULTURA';
+          break;
+        case JWPLAYER:
+          serviceProvider = 'JWPLAYER';
+          break;
+        case VIDEOJS:
+          serviceProvider = 'VIDEOJS';
+          break;
+        default:
+          serviceProvider = 'Unknow';
+          isVideoContainer = false;
+          break;
+      }
 
       searchindContent.forEach( function(item) {
         if( searchingWord( $, item ) ) isVideoContainer = true;
       });
 
       if( isVideoContainer ) {
-      
         
-        var inputRow = [ data.name, data.homepage_url , 'Youtube Service'] ;
-
+        var inputRow = [ data.name, data.homepage_url , serviceProvider ] ;
+        
         writer.write(inputRow);
+
+        console.log('File successfully updated! - Content "' + inputRow + '"');
 
         // If plan use raw txt uncomment below code
         // var inputText = data.name + '(' + data.homepage_url + ')' ;
@@ -90,4 +125,33 @@ function searchingWord( $, word ) {
       return true;
     }
   return false;
+}
+
+function searchingVideoContetn( $ ) {
+  
+  var body = $('html > body');
+  
+  if( body.text().toLowerCase().indexOf('//www.youtube'.toLocaleLowerCase()) !== -1
+      || body.text().toLowerCase().indexOf('//youtube'.toLocaleLowerCase()) !== -1 
+      || $(body).find('[src*="//www.youtube"]').length > 0 || $(body).find('[src*="//.youtube."]').length > 0 
+      || $(body).find('[src*="//youtu.be"]').length > 0 || $(body).find('[src*="//www.youtu.be"]').length > 0 ) {
+    return YOUTUBE;
+  } else if( $(body).find('[src*="//player.vimeo.com"]').length > 0 || $(body).find('[src*="vimeocdn.com/video"]').length > 0 ) {
+    return VIMEO;
+  } else if( $(body).find('[src*="fast.wistia.com"]').length > 0 ) {
+    return WISTIA;
+  } else if( $(body).find('[src*="cdnapi.kaltura.com"]').length > 0 ) {
+    return KULTURA;
+  } else if( $(body).find('[src*="//content.jwplatform.com"]').length > 0  
+      || ( body.text().toLowerCase().indexOf('jwplayer.js'.toLocaleLowerCase()) !== -1 
+            && body.text().toLowerCase().indexOf('jwplayer('.toLocaleLowerCase()) !== -1) 
+      || $(body).find('[src*="//jwpsrv.com"]').length > 0) {
+    return JWPLAYER;
+  } else if( $(body).find('video > source[src*=".mp4"]').length > 0 || $(body).find('video > source[src*=".webm"]').length > 0 
+        || $(body).find('video > source[src*=".ogv"]').length > 0 ) {
+    return VIDEOJS;
+  }
+
+  return false;
+
 }
